12833b61b0820af260ad6786cd390f7929d80ae3
天奇 <4679246@qq.com>
2014-11-14 21:11:17 +0800

    +add sql

--- mysqlhelper.js
+++ mysqlhelper.js
@@ -5,10 +5,10 @@
 var mysql = require('mysql');
 
 var conn = mysql.createConnection({
-    host:'localhost',
+    host:'10.66.123.210',
     user:'root',
-    database:'nodedb',
-    password:'********',
+    database:'fruit_db',
+    password:'AdamGame123',
     port:3306
 });
 conn.connect();
@@ -16,7 +16,7 @@ conn.connect();
 
 function query(client){
     conn.query(
-        'select * from userinfo',
+        'select * from user_info',
         function(err,res,fields){
             console.log(res);
         }
